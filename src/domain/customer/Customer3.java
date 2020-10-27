53b6220f8f3aabe15ee4455ca92b561201d278e5
Anton Stöckl <anton.stoeckl@maibornwolff.de>
2020-10-27 21:09:09 +0100

    Finish test cases and implementation of Customer3

--- src/domain/customer/Customer3.java
+++ src/domain/customer/Customer3.java
@@ -1,25 +1,30 @@
 package domain.customer;
 
+import domain.customer.command.ChangeCustomerEmailAddress;
+import domain.customer.command.ChangeCustomerName;
 import domain.customer.command.ConfirmCustomerEmailAddress;
 import domain.customer.command.RegisterCustomer;
 import domain.customer.value.EmailAddress;
 import domain.customer.value.Hash;
+import domain.customer.value.ID;
 import domain.customer.value.PersonName;
 
 public class Customer3 {
+    private final ID id;
     private EmailAddress emailAddress;
     private Hash confirmationHash;
     private boolean isEmailAddressConfirmed;
     private PersonName name;
 
-    private Customer3(EmailAddress emailAddress, Hash confirmationHash, PersonName name) {
+    private Customer3(ID id, EmailAddress emailAddress, Hash confirmationHash, PersonName name) {
+        this.id = id;
         this.emailAddress = emailAddress;
         this.confirmationHash = confirmationHash;
         this.name = name;
     }
 
     public static Customer3 register(RegisterCustomer command) {
-        return new Customer3(command.emailAddress, command.confirmationHash, command.name);
+        return new Customer3(command.customerID, command.emailAddress, command.confirmationHash, command.name);
     }
 
     public void confirmEmailAddress(ConfirmCustomerEmailAddress command) throws Exception {
@@ -27,6 +32,32 @@ public class Customer3 {
             throw new Exception("confirmation hash does not match");
         }
 
+        if (isEmailAddressConfirmed) {
+            return;
+        }
+
         isEmailAddressConfirmed = true;
+    }
+
+    public void changeEmailAddress(ChangeCustomerEmailAddress command) {
+        if (command.emailAddress.equals(emailAddress)) {
+            return;
+        }
+
+        emailAddress = command.emailAddress;
+        confirmationHash = command.confirmationHash;
+        isEmailAddressConfirmed = false;
+    }
+
+    public void changeName(ChangeCustomerName command) {
+        if (command.name.equals(name)) {
+            return;
+        }
+
+        name = command.name;
+    }
+
+    public CustomerState toCustomerState() {
+        return new CustomerState(id.value, emailAddress.value, confirmationHash.value, name.givenName, name.familyName, isEmailAddressConfirmed);
     }
 }
--- test/domain/customer/Customer3Test.java
+++ test/domain/customer/Customer3Test.java
@@ -1,5 +1,7 @@
 package domain.customer;
 
+import domain.customer.command.ChangeCustomerEmailAddress;
+import domain.customer.command.ChangeCustomerName;
 import domain.customer.command.ConfirmCustomerEmailAddress;
 import domain.customer.command.RegisterCustomer;
 import domain.customer.value.EmailAddress;
@@ -12,21 +14,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Customer3Test {
+    // assigned via givenARegisteredCustomer()
+    private Customer3 registeredCustomer;
     private ID customerID;
+    private Hash confirmationHash;
+
+    // assigned via beforeEach() + used as input in given....() methods and in test cases
+    private PersonName name;
     private EmailAddress emailAddress;
     private EmailAddress changedEmailAddress;
-    private Hash confirmationHash;
     private Hash wrongConfirmationHash;
     private Hash changedConfirmationHash;
-    private PersonName name;
     private PersonName changedName;
 
     @BeforeEach
     public void beforeEach() {
-        customerID = ID.generate();
         emailAddress = EmailAddress.build("john@doe.com");
         changedEmailAddress = EmailAddress.build("john+changed@doe.com");
-        confirmationHash = Hash.generate();
         wrongConfirmationHash = Hash.generate();
         changedConfirmationHash = Hash.generate();
         name = PersonName.build("John", "Doe");
@@ -34,50 +38,217 @@ class Customer3Test {
     }
 
     @Test
-    void registerCustomer() {
-        // Given
-        RegisterCustomer command = RegisterCustomer.build(emailAddress.value, name.givenName, name.familyName);
-
+    public void registerCustomer() {
         // When
-        Customer3 customer = Customer3.register(command);
+        var command = RegisterCustomer.build(emailAddress.value, name.givenName, name.familyName);
+        var customer = Customer3.register(command);
 
         // Then
         assertNotNull(customer);
+
+        // and it should expose the expected state
+        var state = customer.toCustomerState();
+        assertEquals(command.customerID.value, state.id);
+        assertEquals(command.name.givenName, state.givenName);
+        assertEquals(command.name.familyName, state.familyName);
+        assertEquals(command.emailAddress.value, state.emailAddress);
+        assertEquals(command.confirmationHash.value, state.confirmationHash);
+        assertFalse(state.isConfirmed);
     }
 
     @Test
-    void confirmEmailAddress() {
+    public void confirmCustomerEmailAddress() {
         // Given
-        RegisterCustomer register = RegisterCustomer.build(emailAddress.value, name.givenName, name.familyName);
-        Customer3 customer = Customer3.register(register);
-
-        ConfirmCustomerEmailAddress command = ConfirmCustomerEmailAddress.build(register.customerID.value, register.confirmationHash.value);
+        givenARegisteredCustomer();
 
         // When / Then
-        assertDoesNotThrow(()->{customer.confirmEmailAddress(command);});
+        var command = ConfirmCustomerEmailAddress.build(customerID.value, confirmationHash.value);
+        assertDoesNotThrow(() -> registeredCustomer.confirmEmailAddress(command));
 
-        fail();
+        // and the emailAddress should be confirmed
+        var state = registeredCustomer.toCustomerState();
+        assertTrue(state.isConfirmed);
     }
 
     @Test
-    public void confirmEmailAddress_withWrongConfirmationHash() {
+    public void confirmCustomerEmailAddress_withWrongConfirmationHash() {
         // Given
-        RegisterCustomer register = RegisterCustomer.build(emailAddress.value, name.givenName, name.familyName);
-        Customer3 customer = Customer3.register(register);
-
-        ConfirmCustomerEmailAddress command = ConfirmCustomerEmailAddress.build(register.customerID.value, wrongConfirmationHash.value);
+        givenARegisteredCustomer();
 
         // When / Then
-        assertThrows(Exception.class, ()->{customer.confirmEmailAddress(command);});
+        ConfirmCustomerEmailAddress command = ConfirmCustomerEmailAddress.build(customerID.value, wrongConfirmationHash.value);
+        assertThrows(Exception.class, () -> registeredCustomer.confirmEmailAddress(command));
+
+        // and the emailAddress should not be confirmed
+        var state = registeredCustomer.toCustomerState();
+        assertFalse(state.isConfirmed);
     }
 
     @Test
-    public void confirmEmailAddress_whenItWasAlreadyConfirmed() {
+    public void confirmCustomerEmailAddress_whenItWasAlreadyConfirmed() {
+        // Given
+        givenARegisteredCustomer();
+        givenEmailAddressWasConfirmed();
 
+        // When / Then
+        var command = ConfirmCustomerEmailAddress.build(customerID.value, confirmationHash.value);
+        assertDoesNotThrow(() -> registeredCustomer.confirmEmailAddress(command));
+
+        // and the emailAddress should still be confirmed
+        var state = registeredCustomer.toCustomerState();
+        assertTrue(state.isConfirmed);
     }
 
     @Test
-    public void confirmEmailAddress_withWrongConfirmationHash_whenItWasAlreadyConfirmed() {
+    public void confirmCustomerEmailAddress_withWrongConfirmationHash_whenItWasAlreadyConfirmed() {
+        // Given
+        givenARegisteredCustomer();
+        givenEmailAddressWasConfirmed();
 
+        // When / Then
+        var otherCommand = ConfirmCustomerEmailAddress.build(customerID.value, wrongConfirmationHash.value);
+        assertThrows(Exception.class, () -> registeredCustomer.confirmEmailAddress(otherCommand));
+
+        // and the emailAddress should still be confirmed
+        var state = registeredCustomer.toCustomerState();
+        assertTrue(state.isConfirmed);
+    }
+
+    @Test
+    public void changeCustomerEmailAddress() {
+        // Given
+        givenARegisteredCustomer();
+
+        // When / Then
+        var command = ChangeCustomerEmailAddress.build(customerID.value, changedEmailAddress.value);
+        registeredCustomer.changeEmailAddress(command);
+
+        // and the emailAddress and confirmationHash should be changed and the emailAddress should be unconfirmed
+        var state = registeredCustomer.toCustomerState();
+        assertEquals(command.emailAddress.value, state.emailAddress);
+        assertEquals(command.confirmationHash.value, state.confirmationHash);
+        assertFalse(state.isConfirmed);
+    }
+
+    @Test
+    public void changeCustomerEmailAddress_withUnchangedEmailAddress() {
+        // Given
+        givenARegisteredCustomer();
+
+        // When / Then
+        var command = ChangeCustomerEmailAddress.build(customerID.value, emailAddress.value);
+        registeredCustomer.changeEmailAddress(command);
+
+        // and the emailAddress should still be the same
+        var state = registeredCustomer.toCustomerState();
+        assertEquals(emailAddress.value, state.emailAddress);
+    }
+
+    @Test
+    public void changeCustomerEmailAddress_whenItWasAlreadyChanged() {
+        // Given
+        givenARegisteredCustomer();
+        givenCustomerEmailAddressWasChanged();
+
+        // When / Then
+        var command = ChangeCustomerEmailAddress.build(customerID.value, changedEmailAddress.value);
+        registeredCustomer.changeEmailAddress(command);
+
+        // and the emailAddress should still be the same
+        var state = registeredCustomer.toCustomerState();
+        assertEquals(changedEmailAddress.value, state.emailAddress);
+    }
+
+    @Test
+    public void confirmCustomerEmailAddress_whenItWasPreviouslyConfirmedAndThenChanged() {
+        // Given
+        givenARegisteredCustomer();
+        givenEmailAddressWasConfirmed();
+        givenCustomerEmailAddressWasChanged();
+
+        // When // Then
+        var command = ConfirmCustomerEmailAddress.build(customerID.value, changedConfirmationHash.value);
+        assertDoesNotThrow(() -> registeredCustomer.confirmEmailAddress(command));
+
+        // and the emailAddress should be confirmed
+        var state = registeredCustomer.toCustomerState();
+        assertTrue(state.isConfirmed);
+    }
+
+    @Test
+    public void changeCustomerName() {
+        // Given
+        givenARegisteredCustomer();
+
+        // When
+        var command = ChangeCustomerName.build(customerID.value, changedName.givenName, changedName.familyName);
+        registeredCustomer.changeName(command);
+
+        // and it should expose the expected state
+        var state = registeredCustomer.toCustomerState();
+        assertEquals(command.name.givenName, state.givenName);
+        assertEquals(command.name.familyName, state.familyName);
+    }
+
+    @Test
+    public void changeCustomerName_withUnchangedName() {
+        // Given
+        givenARegisteredCustomer();
+
+        // When
+        var command = ChangeCustomerName.build(customerID.value, name.givenName, name.familyName);
+        registeredCustomer.changeName(command);
+
+        // and it should expose the expected state
+        var state = registeredCustomer.toCustomerState();
+        assertEquals(command.name.givenName, state.givenName);
+        assertEquals(command.name.familyName, state.familyName);
+    }
+
+    @Test
+    public void changeCustomerName_whenItWasAlreadyChanged() {
+        // Given
+        givenARegisteredCustomer();
+        givenCustomerNameWasChanged();
+
+        // When
+        var command = ChangeCustomerName.build(customerID.value, changedName.givenName, changedName.familyName);
+        registeredCustomer.changeName(command);
+
+        // and it should expose the expected state
+        var state = registeredCustomer.toCustomerState();
+        assertEquals(command.name.givenName, state.givenName);
+        assertEquals(command.name.familyName, state.familyName);
+    }
+
+    /**
+     * Helper methods to set up the Given state
+     */
+    private void givenARegisteredCustomer() {
+        var register = RegisterCustomer.build(emailAddress.value, name.givenName, name.familyName);
+        customerID = register.customerID;
+        confirmationHash = register.confirmationHash;
+        registeredCustomer = Customer3.register(register);
+    }
+
+    private void givenEmailAddressWasConfirmed() {
+        var command = ConfirmCustomerEmailAddress.build(customerID.value, confirmationHash.value);
+
+        try {
+            registeredCustomer.confirmEmailAddress(command);
+        } catch (Exception e) {
+            fail("unexpected error in givenEmailAddressWasConfirmed: " + e.getMessage());
+        }
+    }
+
+    private void givenCustomerEmailAddressWasChanged() {
+        var command = ChangeCustomerEmailAddress.build(customerID.value, changedEmailAddress.value);
+        changedConfirmationHash = command.confirmationHash;
+        registeredCustomer.changeEmailAddress(command);
+    }
+
+    private void givenCustomerNameWasChanged() {
+        var command = ChangeCustomerName.build(customerID.value, changedName.givenName, changedName.familyName);
+        registeredCustomer.changeName(command);
     }
 }
